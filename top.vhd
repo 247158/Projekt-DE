a525a8924e607ddd7fac6f1c993ba4fb95be930d
247158 <165295428+247158@users.noreply.github.com>
2024-05-01 19:59:05 +0200

    top.vhd

--- top.vhd
+++ top.vhd
@@ -6,69 +6,55 @@ entity top is
     Port (
         CLK100MHZ   : in  std_logic;
         BTNC        : in  std_logic;
+        BTNU        : in  std_logic;
         SW          : in  std_logic_vector(7 downto 0);
         LED         : out std_logic_vector(7 downto 0);
-        CA          : out std_logic; -- Катоды семисегментного индикатора
-        AN          : out std_logic_vector(3 downto 0);
-        caesar_sel  : in  std_logic;
-        vernam_sel  : in  std_logic
+        C           : out std_logic_vector(6 downto 0);
+        DP          : out std_logic;
+        AN          : out std_logic_vector(7 downto 0)
     );
 end top;
 
 architecture Behavioral of top is
-    signal caesar_data   : std_logic_vector(7 downto 0) := (others => '0');
-    signal vernam_data   : std_logic_vector(7 downto 0) := (others => '0');
-    signal caesar_output : std_logic_vector(7 downto 0);
-    signal vernam_output : std_logic_vector(7 downto 0);
-    signal selected_output : std_logic_vector(7 downto 0);
-
-    -- Объявления компонентов шифров...
-
+    component driver_7seg is 
+        port (
+            clk    : in  std_logic;                           
+            rst    : in  std_logic;
+            encoding : in std_logic;
+            code_decode : in std_logic;
+            cipher_en : in std_logic;                    
+            data0  : in  std_logic_vector(5 downto 0);        
+            data1  : in  std_logic_vector(5 downto 0);        
+            data2  : in  std_logic_vector(5 downto 0);        
+            data3  : in  std_logic_vector(5 downto 0);
+            data4  : in  std_logic_vector(5 downto 0);        
+            data5  : in  std_logic_vector(5 downto 0);        
+            data6  : in  std_logic_vector(5 downto 0);        
+            data7  : in  std_logic_vector(5 downto 0);      
+            seg    : out std_logic_vector(6 downto 0);        
+            dig    : out std_logic_vector(7 downto 0)   
+        );
+    end component;
 begin
-
-    -- Инстанцирование шифра Цезаря...
-    caesar_inst: entity work.caesar_cipher
-        port map(
+    DP <= '1';
+    
+    display: component driver_7seg 
+        port map (
             clk => CLK100MHZ,
             rst => BTNC,
-            sw  => caesar_data,
-            led => caesar_output
+            cipher_en => BTNU,
+            encoding => SW(7),
+            code_decode => SW(6),
+            data0 => "000000",
+            data1 => "000001",
+            data2 => "000010",
+            data3 => "000011",
+            data4 => "000100",
+            data5 => "000101",
+            data6 => "000110",
+            data7 => "000111",
+            seg => C,
+            dig => AN
         );
-
-    -- Инстанцирование шифра Вернама...
-    vernam_inst: entity work.vernam_cipher
-        port map(
-            clk => CLK100MHZ,
-            rst => BTNC,
-            sw  => vernam_data,
-            led => vernam_output
-        );
-
-    -- Управляющая логика...
-    process(CLK100MHZ, BTNC)
-    begin
-        if rising_edge(CLK100MHZ) then
-            if BTNC = '1' then  -- Сброс
-                caesar_data <= (others => '0');
-                vernam_data <= (others => '0');
-                selected_output <= (others => '0');
-            else
-                if caesar_sel = '1' then
-                    caesar_data <= SW;
-                    selected_output <= caesar_output;
-                elsif vernam_sel = '1'  then
-                    vernam_data <= SW;
-                    selected_output <= vernam_output;
-                else
-                    selected_output <= SW; -- Default output if no cipher is selected.
-                end if;
-            end if;
-        end if;
-    end process;
-
-    LED <= selected_output;
-
-    -- Инициализация семисегментного дисплея (это только пример)
-    AN <= "1110"; -- Активация только первого сегмента
-    CA <= not SW(0); -- Простой пример вывода значения первого переключателя
+    
 end Behavioral;
