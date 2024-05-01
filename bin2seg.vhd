ba461bda3f331b66eceb862e5f42eec3ac742181
247158 <165295428+247158@users.noreply.github.com>
2024-05-01 20:01:39 +0200

    bin2seg.vhd

--- bin2seg.vhd
+++ bin2seg.vhd
@@ -22,7 +22,7 @@ library ieee;
 entity bin2seg is
     port (
         clear : in    std_logic;                    --! Clear the display
-        bin   : in    std_logic_vector(3 downto 0); --! Binary representation of one hexadecimal symbol
+        bin   : in    std_logic_vector(5 downto 0); --! Binary representation of one hexadecimal symbol
         seg   : out   std_logic_vector(6 downto 0)  --! Seven active-low segments from A to G
     );
 end entity bin2seg;
@@ -47,61 +47,26 @@ begin
 
             case bin is
 
-                when x"0" =>
+                when "000000" =>
                     seg <= "0000001";
-
-                when x"1" =>
+                when "000001" =>
                     seg <= "1001111";
-
-                -- WRITE YOUR CODE HERE
-                -- 2, 3, 4, 5, 6
-
-                when x"2" =>
+                when "000010" =>
                     seg <= "0010010";
-
-                when x"3" =>
+                when "000011" =>
                     seg <= "0000110";
-
-                when x"4" =>
+                when "000100" =>
                     seg <= "1001100";
-
-                when x"5" =>
+                when "000101" =>
                     seg <= "0100100";
-
-                when x"6" =>
+                when "000110" =>
                     seg <= "0100000";
-
-                when x"7" =>
+                when "000111" =>
                     seg <= "0001111";
-
-                when x"8" =>
-                    seg <= "0000000";
-
-                when x"9" =>
-                    seg <= "0000100";
-
-                when x"A" =>
-                    seg <= "0001000";
-
-                when x"b" =>
-                    seg <= "1100000";
-
-                when x"C" =>
-                    seg <= "0110001";
-
-                when x"d" =>
-                    seg <= "1000010";
-
-                when x"E" =>
-                    seg <= "0110000";
-
                 when others =>
                     seg <= "0111000";
-
             end case;
-
         end if;
-
     end process p_7seg_decoder;
 
 end architecture behavioral;
